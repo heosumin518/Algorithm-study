fbd859d2d9a1c242cf7278479fd6fdf707d1c35b
heosumin <heosumin518@gmail.com>
2022-05-16 22:23:29 +0900

    solved 1 basic problems

---
+++ AScode/1238.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("/\\_/\\\n");
+    printf("|q p|\n");
+    printf("{ @ }\"'\"'\"\\\\\\\n");
+    printf("|\"^\"``    | {}\n");
+    printf("||_/'\\\\\\__|\n");
+
+    return 0;
+}

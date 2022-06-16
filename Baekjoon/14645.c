e71f1f116fba9209830d30073affeededb8a1029
heosumin <heosumin518@gmail.com>
2022-06-17 04:07:00 +0900

    solved 10 basic problems

---
+++ Baekjoon/14645.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, k, a, b;
+    scanf("%d %d", &n, &k);
+    while(n--) {
+        scanf("%d %d", &a, &b);
+    }
+    printf("ºñ¿ÍÀÌ\n");
+    return 0;
+}
